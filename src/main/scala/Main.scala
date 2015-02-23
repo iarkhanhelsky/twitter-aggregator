fd64e7d04fa87ebcc9fa23ceab93446e21846174
iarkhanhelsky <ilya.arkhanhelsky[at]gmail.com>
2015-02-23 07:59:10 +0300

    actor test

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,30 @@
+import akka.actor.{Props, ActorSystem, Actor}
+import akka.actor.Actor.Receive
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.Config
 
+import scala.util.Random
+
+class HelloActor extends Actor {
+  override def receive = {
+    case "hello" => println("Mello")
+    case 10 => "Ten"
+    case _ => println("Wello")
+  }
+
+  @throws[Exception](classOf[Exception])
+  override def postStop(): Unit = {
+    super.postStop()
+    println("Bye")
+  }
+}
+
 object Main {
   def main (args: Array[String]) {
-    println("Hello world")
-    val config = ConfigFactory.load()
-    println(config.getString("app-name") + " says " + config.getString("app-mod"))
+    val system = ActorSystem.create("hellosystem")
+    val actor = system.actorOf(Props[HelloActor], "helloactor")
+    actor ! "hello"
+    actor ! "mello"
+    Stream.continually(Random.nextInt(10)).map(actor ! _)
   }
 }
